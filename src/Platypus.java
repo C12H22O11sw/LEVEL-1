26cd36c3a3f354ca11a4511f4abd995bc4722b3e
Mrfuzzy <league@WTS9.attlocal.net>
2016-03-01 17:27:49 -0800

    sbz

---
+++ src/Platypus.java
@@ -0,0 +1,20 @@
+public class Platypus {
+	private String name;
+
+	void sayHi(){
+		System.out.println("The platypus " + name + " is smarter than your average platypus.");
+	}
+	Platypus(String Thename){
+		name=Thename;
+	}
+	public static void main(String[] args) {
+		//1. Make an instance of your new pet platypus
+		Platypus perry = new Platypus("perry");
+		//2. Call the sayHi method
+		perry.sayHi();
+		//3. Create a constructor in the platypus class so that you can give your platypus a name.
+
+		
+	}
+}
+
